34dea0d63e5da5263790c81247133879a54b77bf
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-06-17 22:59:45 -0300

    Correção da fila da direita travada

--- t1-2022.1/t1-2022.1/src/buffet.c
+++ t1-2022.1/t1-2022.1/src/buffet.c
@@ -121,8 +121,8 @@ void buffet_next_step(buffet_t *self, student_t *student)
             int position = student->_buffet_position;
             /* Lock no mutex da próxima posição (bacia) da fila do buffet*/
             pthread_mutex_lock(&self->mutex_queue_right[position + 1]);
-            self->queue_left[position] = 0;
-            self->queue_left[position + 1] = student->_id;
+            self->queue_right[position] = 0;
+            self->queue_right[position + 1] = student->_id;
             if (student->_buffet_position == 0){
                 /* Sempre que a primeira posição da fila de qualquer buffet fica vaga damos um post(incremento) no semafaro da catraca */
                 sem_post(&ratchet);
