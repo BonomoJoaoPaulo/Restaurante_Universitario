9a3ee429bd004e234fb708594a73d341c2902051
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-06-19 14:37:23 -0300

    Refatoração do código do buffet e adição de comentários

--- t1-2022.1/t1-2022.1/src/buffet.c
+++ t1-2022.1/t1-2022.1/src/buffet.c
@@ -5,28 +5,31 @@
 #include "chef.h"
 #include "globals.h"
 
+/* Esse semaforo que funciona como a catraca do RU. */
 sem_t ratchet;
-int cont= 0;
+/*Variavel que conta quantos estudantes ja sairam do buffet. */
+int count;
 
 void *buffet_run(void *arg)
-{   
+{
+    /* Variavel que mostra se todos os students ja entraram no RU ou se ainda ha alguem de fora. */
     int all_students_entered = FALSE;
-    buffet_t *self = (buffet_t*) arg;
+    buffet_t *self = (buffet_t *)arg;
 
-    
     /*  O buffet funciona enquanto houver alunos na fila externa. */
     while (all_students_entered == FALSE)
     {
-        //printf("%d",globals_get_students());
-        if (globals_get_students() == 0) 
+        /* Se o numero de students for 0, 'breaka' o funcionamento do buffet. */
+        if (globals_get_students() == 0)
         {
             break;
         }
         _log_buffet(self);
         msleep(100);
     }
-
-    for(int i = 0; i < 5; i++){
+    /* Como sao 5 mutex (1 para cada posicao no buffet), eh necessario fazer o destroy dentro de um for. */
+    for (int i = 0; i < 5; i++)
+    {
         pthread_mutex_destroy(&self->mutex_queue_left[i]);
         pthread_mutex_destroy(&self->mutex_queue_right[i]);
     }
@@ -35,28 +38,32 @@ void *buffet_run(void *arg)
 
 void buffet_init(buffet_t *self, int number_of_buffets)
 {
+    /* 'Seta' o numero de buffets como o parametro recebido. */
     globals_set_number_of_buffets(number_of_buffets);
+
+    /* i representa o id do buffet e j representa a posicao na fila do buffet (bacia). */
     int i = 0, j = 0;
+
     for (i = 0; i < number_of_buffets; i++)
     {
-        /*A fila possui um ID*/
+        /* A fila possui um ID. */
         self[i]._id = i;
 
-        /* Inicia com 40 unidades de comida em cada bacia */
-        for(j = 0; j < 5; j++)
+        /* Inicia com 40 unidades de comida em cada bacia. */
+        for (j = 0; j < 5; j++)
         {
             self[i]._meal[j] = 40;
 
-        /* Inicia um mutex para cada posição na fila do buffet (cada bacia) */
+            /* Inicia um mutex para cada posicao na fila do buffet (cada bacia). */
             pthread_mutex_init(&self[i].mutex_queue_left[j], NULL);
             pthread_mutex_init(&self[i].mutex_queue_right[j], NULL);
         }
-            
 
-        for(j= 0; j< 5; j++){
-             /* A fila esquerda do buffet possui cinco posições. */
+        for (j = 0; j < 5; j++)
+        {
+            /* A fila esquerda do buffet possui cinco posicoes. */
             self[i].queue_left[j] = 0;
-            /* A fila direita do buffet possui cinco posições. */
+            /* A fila direita do buffet possui cinco posicoes. */
             self[i].queue_right[j] = 0;
         }
 
@@ -64,98 +71,107 @@ void buffet_init(buffet_t *self, int number_of_buffets)
     }
 }
 
-
 int buffet_queue_insert(buffet_t *self, student_t *student)
 {
-    /* Se o estudante vai para a fila esquerda */
-    if (student->left_or_right == 'L') 
+    /* Se o estudante vai para a fila esquerda... */
+    if (student->left_or_right == 'L')
     {
-        /* Verifica se a primeira posição (bacia) do buffet está vaga */
+        /* Verifica se a primeira posicao (bacia) do buffet esta vaga. */
         if (!self[student->_id_buffet].queue_left[0])
-        {   
-            /* Lock no mutex que bloqueia a primeira bacia */
+        {
+            /* Lock no mutex que bloqueia a primeira bacia. */
             pthread_mutex_lock(&self[student->_id_buffet].mutex_queue_left[0]);
             self[student->_id_buffet].queue_left[0] = student->_id;
             student->_buffet_position = 0;
-            //printf("studentL %d", student->_id);
             return 1;
         }
     }
-    /* Se o estudante vai para a fila direita */
+    /* Se o estudante vai para a fila direita... */
     else
-    { 
-        /* Verifica se a primeira posição (bacia) do buffet está vaga */
+    {
+        /* Verifica se a primeira posicao (bacia) do buffet esta vaga. */
         if (!self[student->_id_buffet].queue_right[0])
         {
-            /* Lock no mutex que bloqueia a primeira bacia */
+            /* Lock no mutex que bloqueia a primeira bacia. */
             pthread_mutex_lock(&self[student->_id_buffet].mutex_queue_right[0]);
             self[student->_id_buffet].queue_right[0] = student->_id;
             student->_buffet_position = 0;
-            //printf("studentR %d", student->_id);
             return 1;
         }
     }
     return 0;
 }
 
-
 void buffet_next_step(buffet_t *self, student_t *student)
-{   
+{
     /* Se estudante ainda precisa se servir de mais alguma coisa... */
     if (student->_buffet_position < 4)
-    { 
-        if (student->left_or_right == 'L') /* Está na fila esquerda? */
-        {   /* Caminha para a posição seguinte da fila do buffet.*/
+    {
+        /* Se esta na fila esquerda... */
+        if (student->left_or_right == 'L')
+        {
+            /* Variavel que recebe a posicao atual do estudante. */
             int position = student->_buffet_position;
-            /* Lock no mutex da próxima posição (bacia) da fila do buffet*/
+            /* Lock no mutex da proxima posicao (bacia) da fila do buffet. */
             pthread_mutex_lock(&self[student->_id_buffet].mutex_queue_left[position + 1]);
+            /* Caminha para a posição seguinte da fila do buffet. */
             self[student->_id_buffet].queue_left[position] = 0;
             self[student->_id_buffet].queue_left[position + 1] = student->_id;
-            //printf("did it set?%d\n",buffets_global[student->_id_buffet].queue_left[position+1]);
-            if (student->_buffet_position == 0){
-                /* Sempre que a primeira posição da fila de qualquer buffet fica vaga damos um post(incremento) no semafaro da catraca */
+
+            /* Se ele estiver na primeira posicao da fila do buffet... */
+            if (student->_buffet_position == 0)
+            {
+                /* Sempre que a primeira posicao da fila de qualquer buffet fica vaga damos um post(incremento) no semafaro da catraca. */
                 sem_post(&ratchet);
             }
-            student->_buffet_position ++;
 
-            /* Unlock no mutex da posição (bacia) ATUAL do estudante */
+            student->_buffet_position++;
+
+            /* Unlock no mutex da posição (bacia) ATUAL do estudante. */
             pthread_mutex_unlock(&self[student->_id_buffet].mutex_queue_left[position]);
-            //printf("student %d deu unlock L : %d \n", student->_id, position);
-
-        } else /* Está na fila direita? */
-        {   /* Caminha para a posição seguinte da fila do buffet.*/
+        }
+        /* Se esta na fila da direita...*/
+        /* Mesma logica da fila da esquerda. */
+        else
+        {
             int position = student->_buffet_position;
-            /* Lock no mutex da próxima posição (bacia) da fila do buffet*/
+
             pthread_mutex_lock(&self[student->_id_buffet].mutex_queue_right[position + 1]);
+
             self[student->_id_buffet].queue_right[position] = 0;
             self[student->_id_buffet].queue_right[position + 1] = student->_id;
-            if (student->_buffet_position == 0){
-                /* Sempre que a primeira posição da fila de qualquer buffet fica vaga damos um post(incremento) no semafaro da catraca */
-                // printf("deu post\n");
+
+            if (student->_buffet_position == 0)
+            {
                 sem_post(&ratchet);
             }
-            student->_buffet_position ++;
-            /* Unlock no mutex da posição (bacia) ATUAL do estudante */
+
+            student->_buffet_position++;
+
             pthread_mutex_unlock(&self[student->_id_buffet].mutex_queue_right[position]);
-            //printf("student %d deu unlock R : %d \n", student->_id, position);
         }
-    } else /* Caso o estudante esteja na última bacia */
+    }
+    /* Caso o estudante esteja na última bacia... */
+    else
     {
-        if (student->left_or_right == 'L') {
-            /* Unlock no mutex da bacia que o estudante está (a última) */
+        if (student->left_or_right == 'L')
+        {
+            /* Unlock no mutex da bacia que o estudante esta (a ultima). */
             self[student->_id_buffet].queue_left[4] = 0;
             pthread_mutex_unlock(&self[student->_id_buffet].mutex_queue_left[4]);
-        } else {
-            /* Unlock no mutex da bacia que o estudante está (a última) */
+        }
+        else
+        {
+            /* Unlock no mutex da bacia que o estudante esta (a ultima). */
             self[student->_id_buffet].queue_right[4] = 0;
             pthread_mutex_unlock(&self[student->_id_buffet].mutex_queue_right[4]);
         }
 
-        /* Define a posição do estudante no buffet como -1 (fora) */
+        /* Define a posicao do estudante no buffet como -1 (fora). */
         student->_buffet_position = -1;
         printf("O student %d saiu do buffet. \n", student->_id);
-        cont++;
-        //printf("%d", cont);
+        count++;
+        printf("Students ja servidos: %d", count);
     }
 }
 
@@ -170,23 +186,22 @@ void buffet_finalize(buffet_t *self, int number_of_buffets)
     {
         pthread_join(self[i].thread, NULL);
     }
-    
+
     /*Libera a memória.*/
     free(self);
 }
 
-
 void _log_buffet(buffet_t *self)
 {
     /* Prints do buffet */
-    int *ids_left = self->queue_left; 
-    int *ids_right = self->queue_right; 
+    int *ids_left = self->queue_left;
+    int *ids_right = self->queue_right;
 
-    printf("\n\n\u250F\u2501 Queue left: [ %d %d %d %d %d ]\n", ids_left[0],ids_left[1],ids_left[2],ids_left[3],ids_left[4]);
+    printf("\n\n\u250F\u2501 Queue left: [ %d %d %d %d %d ]\n", ids_left[0], ids_left[1], ids_left[2], ids_left[3], ids_left[4]);
     fflush(stdout);
     printf("\u2523\u2501 BUFFET %d = [RICE: %d/40 BEANS:%d/40 PLUS:%d/40 PROTEIN:%d/40 SALAD:%d/40]\n",
            self->_id, self->_meal[0], self->_meal[1], self->_meal[2], self->_meal[3], self->_meal[4]);
     fflush(stdout);
-    printf("\u2517\u2501 Queue right: [ %d %d %d %d %d ]\n", ids_right[0],ids_right[1],ids_right[2],ids_right[3],ids_right[4]);
+    printf("\u2517\u2501 Queue right: [ %d %d %d %d %d ]\n", ids_right[0], ids_right[1], ids_right[2], ids_right[3], ids_right[4]);
     fflush(stdout);
 }
--- t1-2022.1/t1-2022.1/src/worker_gate.c
+++ t1-2022.1/t1-2022.1/src/worker_gate.c
@@ -6,30 +6,28 @@
 #include "globals.h"
 #include "config.h"
 
-pthread_mutex_t buffet_first_position_mutex; //mutex para impedir que estudantes diferentes queiram ser inseridos na mesma primeira posicao de um buffet
+pthread_mutex_t buffet_first_position_mutex; // mutex para impedir que estudantes diferentes queiram ser inseridos na mesma primeira posicao de um buffet
 pthread_mutex_t remove_student_mutex;
-sem_t ratchet;  // semafaro catraca para o worker gate so procurar posicoes livres nas filas do buffet quando elas existirem(evita espera ocupada)
-
+sem_t ratchet; // semafaro catraca para o worker gate so procurar posicoes livres nas filas do buffet quando elas existirem(evita espera ocupada)
 
 int worker_gate_look_queue()
-{   queue_t *students_queue = globals_get_queue();
+{
+    queue_t *students_queue = globals_get_queue();
     if (students_queue->_length == 0)
     {
         printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        return 0;
-    } else
-    {
         return 1;
     }
 
+    return 0;
 }
 
 void worker_gate_remove_student()
-{   
+{
     queue_t *students_queue = globals_get_queue();
     msleep(500);
     if (students_queue->_length != 0)
-    {   
+    {
         printf("tamanho da fila %d\n", students_queue->_length);
         printf("primeiro estudante removido\n");
         pthread_mutex_lock(&remove_student_mutex);
@@ -40,7 +38,7 @@ void worker_gate_remove_student()
 
 void worker_gate_look_buffet()
 {
- //nao esta sendo usado
+    // nao esta sendo usado
 }
 
 void *worker_gate_run(void *arg)
@@ -55,32 +53,30 @@ void *worker_gate_run(void *arg)
     pthread_mutex_init(&remove_student_mutex, NULL);
 
     int number_of_buffets = globals_get_number_of_buffets();
-    //inicializar o mutex do worker gate init queue
+    // inicializar o mutex do worker gate init queue
 
-   
-    sem_init(&ratchet, 0, number_of_buffets*2);// inicializando ele com numero de buffets x 2 porque tem 2 filas
-    int sval2;    
+    sem_init(&ratchet, 0, number_of_buffets * 2); // inicializando ele com numero de buffets x 2 porque tem 2 filas
+    int sval2;
     sem_getvalue(&ratchet, &sval2);
-    //printf("Semaphore value: %d\n", sval2); 
-
+    // printf("Semaphore value: %d\n", sval2);
 
     while (all_students_entered == FALSE)
     {
-        
+
         worker_gate_look_buffet();
         worker_gate_remove_student();
-        worker_gate_look_queue();
+        int students_on_queue = worker_gate_look_queue();
         int number_of_students = globals_get_students();
-        if (number_of_students == 0){
+        if (students_on_queue != 0)
+        {
             break;
         }
-        //msleep(5000); /* Pode retirar este sleep quando implementar a solução! */
+        // msleep(5000); /* Pode retirar este sleep quando implementar a solução! */
     }
 
     pthread_mutex_destroy(&buffet_first_position_mutex);
     pthread_mutex_destroy(&remove_student_mutex);
     pthread_exit(NULL);
-    
 }
 
 void worker_gate_init(worker_gate_t *self)
@@ -98,34 +94,36 @@ void worker_gate_finalize(worker_gate_t *self)
 
 void worker_gate_insert_queue_buffet(student_t *student)
 {
-    //printf("chamou a funcao");
-    //queue_t *students_queue = globals_get_queue();
-    //queue_insert(students_queue, student);
+    // printf("chamou a funcao");
+    // queue_t *students_queue = globals_get_queue();
+    // queue_insert(students_queue, student);
     buffet_t *buffets = globals_get_buffets();
     int number_of_buffets = globals_get_number_of_buffets();
     int sval2;
     sem_getvalue(&ratchet, &sval2);
     printf("Semaphore value: %d\n", sval2);
-    sem_wait(&ratchet);// semafaro que evita espera ocupada
-    pthread_mutex_lock(&buffet_first_position_mutex);  
+    sem_wait(&ratchet); // semafaro que evita espera ocupada
+    pthread_mutex_lock(&buffet_first_position_mutex);
     for (int i = 0; i < number_of_buffets; i++)
     {
         int number_of_students = globals_get_students();
-        if (buffets[i].queue_left[0] == 0){
+        if (buffets[i].queue_left[0] == 0)
+        {
             student->_id_buffet = buffets[i]._id;
             student->left_or_right = 'L';
-            buffet_queue_insert(buffets,student);
+            buffet_queue_insert(buffets, student);
             pthread_mutex_unlock(&buffet_first_position_mutex);
-            printf("ugauga %d\n",number_of_students);
+            printf("ugauga %d\n", number_of_students);
             break;
-        }if (buffets[i].queue_right[0] == 0){
+        }
+        if (buffets[i].queue_right[0] == 0)
+        {
             student->_id_buffet = buffets[i]._id;
             student->left_or_right = 'R';
-            buffet_queue_insert(buffets,student);
+            buffet_queue_insert(buffets, student);
             pthread_mutex_unlock(&buffet_first_position_mutex);
-            printf("ugauga %d\n",number_of_students);
+            printf("ugauga %d\n", number_of_students);
             break;
         }
     }
-    
 }
